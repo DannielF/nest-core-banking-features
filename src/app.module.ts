fce94876e43202921811e5a93510c2d749c3d3cb
daniel.granados <daniel.granados@sofka.com.co>
2023-04-05 15:45:41 -0500

    Add: configService .env and cors default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './enviroment/.env.local',
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { config } from './config/swaggerConfig/swaggerConfig';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
