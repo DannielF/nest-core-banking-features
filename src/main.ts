752ca9b5206d56c3ae450162e69a3c173b24d22f
daniel.granados <daniel.granados@sofka.com.co>
2023-04-11 11:44:29 -0500

    Upd: main

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 
-import { config } from './config/swaggerConfig/swaggerConfig';
+import { config as swaggerConfig } from './config/swaggerConfig/swaggerConfig';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
-  const document = SwaggerModule.createDocument(app, config);
+  const config = app.get<ConfigService>('ConfigService');
+  const port = config.get('PORT');
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
-  logger.log(`Application listening on port ${3000}`);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
