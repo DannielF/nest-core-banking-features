c34f05d566450cf683d448b809506a07afe34875
daniel.granados <daniel.granados@sofka.com.co>
2023-04-13 11:00:46 -0500

    Fix: imports onboarding module

--- src/onboarding/onboarding.module.ts
+++ src/onboarding/onboarding.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingController } from './onboarding.controller';
-import { ClientService } from 'src/clients/client.service';
-import { AccountService } from 'src/accounts/account.service';
 import { HeaderModule } from 'src/config/header/header.module';
+import { ClientModule } from 'src/clients/client.module';
+import { AccountModule } from 'src/accounts/account.module';
 
 @Module({
   controllers: [OnboardingController],
   providers: [OnboardingService],
-  imports: [ClientService, AccountService, HeaderModule],
+  imports: [ClientModule, AccountModule, HeaderModule],
 })
 export class OnboardingModule {}
