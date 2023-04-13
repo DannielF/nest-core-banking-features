875fa64c1e85ad9725f1dde34b72009e9ad23f3f
daniel.granados <daniel.granados@sofka.com.co>
2023-04-13 11:01:31 -0500

    Del: onboarding module some import

--- src/onboarding/onboarding.module.ts
+++ src/onboarding/onboarding.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingController } from './onboarding.controller';
-import { HeaderModule } from 'src/config/header/header.module';
 import { ClientModule } from 'src/clients/client.module';
 import { AccountModule } from 'src/accounts/account.module';
 
 @Module({
   controllers: [OnboardingController],
   providers: [OnboardingService],
-  imports: [ClientModule, AccountModule, HeaderModule],
+  imports: [ClientModule, AccountModule],
 })
 export class OnboardingModule {}
