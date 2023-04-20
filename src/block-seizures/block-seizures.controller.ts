409a83cfa863061784d619aac2e05bfb7d2e3fb3
daniel.granados <daniel.granados@sofka.com.co>
2023-04-20 12:39:03 -0500

    Upd: block-seizes handle exceptions

--- src/block-seizures/block-seizures.controller.ts
+++ src/block-seizures/block-seizures.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ErrorResponse } from 'src/common/models/error-response.entity';
 import { BlockSeizureService } from './block-seizures.service';
+import { CreateAccountChangeStateDto } from './dto/create-account-change-state.dto';
 import { CreateBlockFundsDto } from './dto/create-block-funds.dto';
 import { CreateSeizureFundsDto } from './dto/create-siezure-funds.dto';
-import { CreateAccountChangeStateDto } from './dto/create-account-change-state.dto';
-import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Block-seizures')
 @Controller('block-seizures')
@@ -14,42 +23,123 @@ export class BlockSeizuresController {
   @ApiParam({ name: 'depositAccountId', type: 'string', required: true })
   @ApiBody({ type: CreateBlockFundsDto, required: true })
   @Post('blocks/:depositAccountId')
-  blocks(
+  async blocks(
     @Param('depositAccountId') id: string,
     @Body() createBlockSeizureDto: CreateBlockFundsDto,
   ) {
-    return this.blockSeizureService.blockFunds(id, createBlockSeizureDto);
+    return await this.blockSeizureService
+      .blockFunds(id, createBlockSeizureDto)
+      .then((response) => {
+        if (response.accountKey === undefined) {
+          throw new HttpException(response, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        const errorMambu: ErrorResponse = error.response;
+        throw new HttpException(
+          {
+            status: errorMambu.errors[0].errorCode,
+            message: errorMambu.errors[0].errorReason,
+            source: errorMambu.errors.map((error) => error.errorSource),
+          },
+          HttpStatus.BAD_REQUEST,
+          {
+            cause: error,
+          },
+        );
+      });
   }
 
   @ApiOperation({ summary: 'Seizure funds' })
   @ApiParam({ name: 'depositAccountId', type: 'string', required: true })
   @ApiBody({ type: CreateSeizureFundsDto, required: true })
   @Post('seizures/:depositAccountId')
-  seizure(
+  async seizure(
     @Param('depositAccountId') id: string,
     @Body() createBlockSeizureDto: CreateSeizureFundsDto,
   ) {
-    return this.blockSeizureService.seizureFunds(id, createBlockSeizureDto);
+    return await this.blockSeizureService
+      .seizureFunds(id, createBlockSeizureDto)
+      .then((response) => {
+        if (response.errors) {
+          throw new HttpException(response, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        const errorMambu: ErrorResponse = error.response;
+        throw new HttpException(
+          {
+            status: errorMambu.errors[0].errorCode,
+            message: errorMambu.errors[0].errorReason,
+            source: errorMambu.errors.map((error) => error.errorSource),
+          },
+          HttpStatus.BAD_REQUEST,
+          {
+            cause: error,
+          },
+        );
+      });
   }
 
   @ApiOperation({ summary: 'Change state account' })
   @ApiParam({ name: 'depositAccountId', type: 'string', required: true })
   @ApiBody({ type: CreateAccountChangeStateDto, required: true })
   @Post('change-state/:depositAccountId')
-  changeState(
+  async changeState(
     @Param('depositAccountId') id: string,
     @Body() createBlockSeizureDto: CreateAccountChangeStateDto,
   ) {
-    return this.blockSeizureService.accountChangeState(
-      id,
-      createBlockSeizureDto,
-    );
+    return await this.blockSeizureService
+      .accountChangeState(id, createBlockSeizureDto)
+      .then((response) => {
+        if (response.errors) {
+          throw new HttpException(response, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        const errorMambu: ErrorResponse = error.response;
+        throw new HttpException(
+          {
+            status: errorMambu.errors[0].errorCode,
+            message: errorMambu.errors[0].errorReason,
+            source: errorMambu.errors.map((error) => error.errorSource),
+          },
+          HttpStatus.BAD_REQUEST,
+          {
+            cause: error,
+          },
+        );
+      });
   }
 
   @ApiOperation({ summary: 'Get all blocks funds' })
   @ApiParam({ name: 'depositAccountId', type: 'string', required: true })
   @Get('all-blocks/:depositAccountId')
-  getAllBlocksFunds(@Param('depositAccountId') id: string) {
-    return this.blockSeizureService.AllBlockFunds(id);
+  async getAllBlocksFunds(@Param('depositAccountId') id: string) {
+    return await this.blockSeizureService
+      .AllBlockFunds(id)
+      .then((response) => {
+        if (response.errors) {
+          throw new HttpException(response, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        const errorMambu: ErrorResponse = error.response;
+        throw new HttpException(
+          {
+            status: errorMambu.errors[0].errorCode,
+            message: errorMambu.errors[0].errorReason,
+            source: errorMambu.errors.map((error) => error.errorSource),
+          },
+          HttpStatus.BAD_REQUEST,
+          {
+            cause: error,
+          },
+        );
+      });
   }
 }
