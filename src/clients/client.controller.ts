db754f71af76a74655d66ceae678d5c5ec3a9832
daniel.granados <daniel.granados@sofka.com.co>
2023-04-21 08:58:10 -0500

    Upd: client exception handler

--- src/clients/client.controller.ts
+++ src/clients/client.controller.ts
@@ -1,14 +1,5 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
-import { ErrorResponse } from 'src/common/models/error-response.entity';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 
@@ -20,56 +11,14 @@ export class ClientController {
   @ApiOperation({ summary: 'Create client' })
   @ApiBody({ type: CreateClientDto, required: true })
   @Post()
-  async create(@Body() createClientDto: CreateClientDto) {
-    return await this.clientService
-      .create(createClientDto)
-      .then((response) => {
-        if (response.encodedKey === undefined) {
-          throw new HttpException(response, HttpStatus.BAD_REQUEST);
-        }
-        return response;
-      })
-      .catch((error) => {
-        const errorMambu: ErrorResponse = error.response;
-        throw new HttpException(
-          {
-            status: errorMambu.errors[0].errorCode,
-            message: errorMambu.errors[0].errorReason,
-            source: errorMambu.errors.map((error) => error.errorSource),
-          },
-          HttpStatus.BAD_REQUEST,
-          {
-            cause: error,
-          },
-        );
-      });
+  create(@Body() createClientDto: CreateClientDto) {
+    return this.clientService.create(createClientDto);
   }
 
   @ApiOperation({ summary: 'Get client by id' })
   @ApiParam({ name: 'id', type: 'string', required: true })
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.clientService
-      .getClientById(id)
-      .then((response) => {
-        if (response.encodedKey === undefined) {
-          throw new HttpException(response, HttpStatus.BAD_REQUEST);
-        }
-        return response;
-      })
-      .catch((error) => {
-        const errorMambu: ErrorResponse = error.response;
-        throw new HttpException(
-          {
-            status: errorMambu.errors[0].errorCode,
-            message: errorMambu.errors[0].errorReason,
-            source: errorMambu.errors.map((error) => error.errorSource),
-          },
-          HttpStatus.BAD_REQUEST,
-          {
-            cause: error,
-          },
-        );
-      });
+  findOne(@Param('id') id: string) {
+    return this.clientService.getClientById(id);
   }
 }
--- src/clients/client.service.ts
+++ src/clients/client.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { CreateClientDto } from './dto/create-client.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { HeaderService } from 'src/config/header/header.config';
+import { CreateClientDto } from './dto/create-client.dto';
 import { ResponseCreateClient } from './entities/response-create-client.entity';
 
 @Injectable()
@@ -10,23 +10,51 @@ export class ClientService {
   async create(
     createClientDto: CreateClientDto,
   ): Promise<ResponseCreateClient> {
-    const response = await fetch(`${this.headerService.baseUrl}/clients/`, {
+    return await fetch(`${this.headerService.baseUrl}/clients/`, {
       method: 'POST',
       headers: this.headerService.headers,
       body: JSON.stringify(createClientDto),
-    });
-    return await response.json();
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.errors) {
+          throw new HttpException(response.errors, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        throw new HttpException(
+          {
+            action: 'Check the data sent to the endpoint',
+            mambuError: error.response,
+          },
+          HttpStatus.BAD_REQUEST,
+          { cause: error },
+        );
+      });
   }
 
   async getClientById(id: string) {
     const { Accept, Authorization } = this.headerService.headers;
-    const response = await fetch(
-      `${this.headerService.baseUrl}/clients/${id}`,
-      {
-        method: 'GET',
-        headers: { Accept, Authorization, 'Content-type': 'application/json' },
-      },
-    );
-    return await response.json();
+    return await fetch(`${this.headerService.baseUrl}/clients/${id}`, {
+      method: 'GET',
+      headers: { Accept, Authorization, 'Content-type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.errors) {
+          throw new HttpException(response.errors, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+      })
+      .catch((error) => {
+        throw new HttpException(
+          {
+            mambuError: error.response,
+          },
+          HttpStatus.BAD_REQUEST,
+          { cause: error },
+        );
+      });
   }
 }
